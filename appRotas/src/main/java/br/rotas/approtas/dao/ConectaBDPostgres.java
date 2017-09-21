d191f6690eda6b7bf8522496f5a7bc85efd63f09
Alencar Machado <alencar.machado@ufsm.br>
2017-09-21 15:12:04 -0300

    Listar clientes

--- appRotas/src/main/java/br/rotas/approtas/dao/ConectaBDPostgres.java
+++ appRotas/src/main/java/br/rotas/approtas/dao/ConectaBDPostgres.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
 
 public class ConectaBDPostgres {
 	
+        private static final String DB_TESTE = "db_rotas2";
+        private static final String DB_PRODUCAO = "db_rotas2_producao";
+    
 	public static void main(String args[]){
 		getConexao();
 		System.out.println("... conexão ok");
@@ -19,7 +22,7 @@ public class ConectaBDPostgres {
 		try {
 			Class.forName("org.postgresql.Driver");
 		
-			String url ="jdbc:postgresql://200.132.36.170:5432/db_rotas2";
+			String url ="jdbc:postgresql://200.132.36.170:5432/"+DB_TESTE;
                         String user="postgres";
 			String password = "amj!#7";
 			
