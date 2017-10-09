cd543391b1d51ced30ff9e8ae6037b42598828ab
Alencar Machado <alencar.machado@ufsm.br>
2017-10-09 16:59:03 -0300

    atualicação nas daos

--- appRotas/src/main/java/br/rotas/approtas/dao/ConectaBDPostgres.java
+++ appRotas/src/main/java/br/rotas/approtas/dao/ConectaBDPostgres.java
@@ -22,9 +22,9 @@ public class ConectaBDPostgres {
 		try {
 			Class.forName("org.postgresql.Driver");
 		
-			String url ="jdbc:postgresql://+:5432/"+DB_TESTE;
+			String url ="jdbc:postgresql://=:5432/"+DB_TESTE;
                         String user="postgres";
-			String password = "+";
+			String password = "8";
 			
 			c = DriverManager.getConnection(url, user, password);
 		} catch (Exception e) { 
