27c42100baf30c4eac7553805dcb3013bf294f22
Alencar Machado <alencar.machado@ufsm.br>
2017-10-24 08:25:46 -0200

    atualicação nas daos

--- appRotas/src/main/java/br/rotas/approtas/dao/ConectaBDPostgres.java
+++ appRotas/src/main/java/br/rotas/approtas/dao/ConectaBDPostgres.java
@@ -4,32 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class ConectaBDPostgres {
-	
-        private static final String DB_TESTE = "db_rotas2_developer";
-        private static final String DB_PRODUCAO = "db_rotas2_producao";
-    
-	public static void main(String args[]){
-		getConexao();
-		System.out.println("... conexão ok");
-	}
-	
-	
-	
-	public static Connection getConexao(){
-		Connection c = null;
-		try {
-			Class.forName("org.postgresql.Driver");
-		
-			String url ="jdbc:postgresql://+:5432/"+DB_TESTE;
-                        String user="postgres";
-			String password = "-";
-			
-			c = DriverManager.getConnection(url, user, password);
-		} catch (Exception e) { 
-			e.printStackTrace();
-			}
-		return c;
-	}
+
+    private static final String DB_TESTE = "db_rotas2_developer";
+    private static final String DB_PRODUCAO = "db_rotas2_producao";
+
+    public static void main(String args[]) {
+        getConexao();
+        System.out.println("... conexão ok");
+    }
+
+    public static Connection getConexao() {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            String url = "jdbc:postgresql://+:5432/" + DB_TESTE;
+            String user = "postgres";
+            String password = "-";
+
+            c = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
 }
